3f00f0912a4c003e8e406ef8559088ef69ad2df0
yingchaojie <yingchaojie@163.com>
2020-07-08 11:49:50 +0800

    test

--- src/plugins/composition-api.ts
+++ src/plugins/composition-api.ts
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
 Vue.use(VueCompositionApi);
 
-declare module '@vue/composition-api/dist/component/component' {
-  interface SetupContext {
-    readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] };
-  }
-}
+// declare module '@vue/composition-api/dist/component/component' {
+//   interface SetupContext {
+//     readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] };
+//   }
+// }
--- src/service/use-cache.ts
+++ src/service/use-cache.ts
@@ -1,5 +1,5 @@
 import { isUndefined } from '../utils';
-import { ref, Ref } from '@vue/composition-api';
+import { ref } from '@vue/composition-api';
 
 const legalType = ['string', 'number', 'boolean', 'object'];
 const PREFIX = 'use-cache-';
@@ -46,53 +46,53 @@ interface ICacheOptions {
  * @param initValue
  * @param opts
  */
-export function useCache<T>(id: string, initValue: T, opts: Partial<ICacheOptions> = {}): [Ref<T>, (val: T) => T] {
-  const storage = opts.storage || 'session';
-  let store: Storage;
-  switch (storage) {
-    case 'local': store = localStorage; break;
-    case 'session': store = sessionStorage; break;
-    case 'memory': store = memoryStorage; break;
-    default: store = sessionStorage; break;
-  }
-  const expire = isUndefined(opts.expire) ? 0 : opts.expire;
-  const type = typeof initValue;
-  if (!legalType.includes(type)) {
-    throw new TypeError(`useCache: Illegal type <${type}> of ${id}.`);
-  }
-  const cache = store.getItem(PREFIX + id);
-  const lastModified = store.getItem(PREFIX + 'last-modified-' + id);
-  let data: Ref<T>;
-  if (cache !== null && (!expire || lastModified && Date.now() - new Date(lastModified).getTime() < expire)) {
-    if (type === 'object') {
-      try {
-        data = ref(JSON.parse(cache));
-      } catch (e) {
-        console.error(new SyntaxError(`In useCache: cache <${id}> is <${type}>, but JSON.parse cannot parse it correctly.`));
-        throw e;
-      }
-    } else {
-      data = ref(parseValueType(cache, type));
-    }
-  } else {
-    data = ref(initValue);
-  }
-  function update(val: T) {
-    let value: string;
-    if (type === 'object') {
-      value = val ? JSON.stringify(val) : '';
-    } else {
-      value = String(val);
-    }
-    store.setItem(PREFIX + id, value);
-    if (expire) {
-      store.setItem(PREFIX + 'last-modified-' + id, Date.now().toString());
-    }
-    data.value = val;
-    return val;
-  }
-  return [data, update];
-}
+// export function useCache(id: string, initValue: any, opts: Partial<ICacheOptions> = {}): [Ref<T>, (val: T) => T] {
+//   const storage = opts.storage || 'session';
+//   let store: Storage;
+//   switch (storage) {
+//     case 'local': store = localStorage; break;
+//     case 'session': store = sessionStorage; break;
+//     case 'memory': store = memoryStorage; break;
+//     default: store = sessionStorage; break;
+//   }
+//   const expire = isUndefined(opts.expire) ? 0 : opts.expire;
+//   const type = typeof initValue;
+//   if (!legalType.includes(type)) {
+//     throw new TypeError(`useCache: Illegal type <${type}> of ${id}.`);
+//   }
+//   const cache = store.getItem(PREFIX + id);
+//   const lastModified = store.getItem(PREFIX + 'last-modified-' + id);
+//   let data: Ref<T>;
+//   if (cache !== null && (!expire || lastModified && Date.now() - new Date(lastModified).getTime() < expire)) {
+//     if (type === 'object') {
+//       try {
+//         data = ref(JSON.parse(cache));
+//       } catch (e) {
+//         console.error(new SyntaxError(`In useCache: cache <${id}> is <${type}>, but JSON.parse cannot parse it correctly.`));
+//         throw e;
+//       }
+//     } else {
+//       data = ref(parseValueType(cache, type));
+//     }
+//   } else {
+//     data = ref(initValue);
+//   }
+//   function update(val: T) {
+//     let value: string;
+//     if (type === 'object') {
+//       value = val ? JSON.stringify(val) : '';
+//     } else {
+//       value = String(val);
+//     }
+//     store.setItem(PREFIX + id, value);
+//     if (expire) {
+//       store.setItem(PREFIX + 'last-modified-' + id, Date.now().toString());
+//     }
+//     data.value = val;
+//     return val;
+//   }
+//   return [data, update];
+// }
 function parseValueType(str: string, type: string) {
   switch (type) {
     case 'string':
