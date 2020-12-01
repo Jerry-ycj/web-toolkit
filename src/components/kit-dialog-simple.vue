445bc1bb119be90ec9921fa7d607608eff10572a
yingchaojie <yingchaojie@163.com>
2020-12-01 10:49:02 +0800

    update

--- src/components/kit-dialog-simple.vue
+++ src/components/kit-dialog-simple.vue
@@ -1,5 +1,6 @@
 <template>
   <el-dialog
+      v-dialogDrag
           :modal-append-to-body='false'
           :close-on-click-modal="false"
           :visible.sync="modal.visible"
@@ -24,6 +25,8 @@
 import { ref} from '@vue/composition-api';
 import { submitErrChanel, clearErrMsg} from '../case-main';
 import {useLoading} from '../service';
+import 'web-toolkit/src/directive/dialog-drag';
+
 export default {
   props: {
     confirm: {
