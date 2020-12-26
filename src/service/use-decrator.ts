b5e042ee21336f3aba050f1ca197e7069a4daa09
yingchaojie <yingchaojie@163.com>
2020-12-26 10:19:02 +0800

    update

--- src/service/use-decrator.ts
+++ src/service/use-decrator.ts
@@ -5,7 +5,7 @@ import {AnyFunction} from '../types/common';
 export function useLoading(loadingValue: Ref<boolean>, fn: AnyFunction) {
   // 环境变量不同
   // return useLoadingDirect(loadingValue.value, fn);
-  return async function (this: any, ...rest: any[]) {
+  return async function(this: any, ...rest: any[]) {
     loadingValue.value = true;
     try {
       await fn.apply(this, rest);
@@ -22,7 +22,7 @@ export function useLoadingDirect(loadingModal: Ref<any>, fn: AnyFunction) {
   if (!loadingModal.value) {
     throw Error('useLoadingDirect: loadingModal is null');
   }
-  return async function (this: any, ...rest: any[]) {
+  return async function(this: any, ...rest: any[]) {
     loadingModal.value.loading = true;
     try {
       await fn.apply(this, rest);
@@ -42,27 +42,27 @@ export function useConfirm(msg: string, fn: AnyFunction) {
   };
 }
 export function useConfirmNew(fn: AnyFunction, msg?: string) {
-  return async function (this: any, ...rest: any[]) {
+  return async function(this: any, ...rest: any[]) {
     try {
-      const h2 = this.$createElement
+      const h2 = this.$createElement;
       await MessageBox({
         message: h2('div', {style: 'margin: 20px;font-size: 15px;font-weight: 1200;'}, [h2('div', {
             class: 'flex align-center little-space',
-            style: 'font-size: 18px;font-weight: bold;'
+            style: 'font-size: 18px;font-weight: bold;',
           }, [h2('i', {
             class: 'el-icon-warning-outline',
-            style: 'margin-bottom:10px;font-size: 30px;color: #F5A400;'
+            style: 'margin-bottom:10px;font-size: 30px;color: #F5A400;',
           }, ''), h2('p', '你确定删除这项内容吗？', {style: 'margin-top:-20px;font-size: 18px;font-weight: bold;'})]),
-            h2('div', {style: 'margin-left:40px;font-size: 14px;'}, msg?msg:'删除这条数据后，将无法恢复')]
+            h2('div', {style: 'margin-left:40px;font-size: 14px;'}, msg ? msg : '删除这条数据后，将无法恢复')],
         ),
-        closeOnClickModal:false,
-        closeOnPressEscape:false,
+        closeOnClickModal: false,
+        closeOnPressEscape: false,
         showClose: false,
         showConfirmButton: true,
         confirmButtonText: '删除',
         confirmButtonClass: 'button-delete',
-        showCancelButton: true
-      })
+        showCancelButton: true,
+      });
       fn.apply(this, rest);
     } catch {
     }
@@ -74,7 +74,7 @@ export function useLoadingStore(store: any, fn: AnyFunction) {
   if (!store) {
     throw Error('useLoadingStore: store is null');
   }
-  return async function (this: any, ...rest: any[]) {
+  return async function(this: any, ...rest: any[]) {
     store.loading = true;
     try {
       await fn.apply(this, rest);
